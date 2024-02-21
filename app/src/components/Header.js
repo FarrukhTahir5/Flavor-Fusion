4aeaab69e1bbf8c9ecd1046941900df3b79e6d37
farrukhtahir5 <farrukhtahir5@gmail.com>
2024-02-21 21:38:12 +0500

    Made Header - Styled it - Day 1

---
+++ app/src/components/Header.js
@@ -0,0 +1,13 @@
+import logo from '../icon.png';
+export default function Header() {
+    return (<><div className="header-brand">
+    <img className="header-icon" src={logo}/>
+    <div className="title">Flavor Fusion</div>
+    </div>
+  <ul className="header-list">
+    <li><a href="./">Home</a></li>
+    <li><a href="./">Menu</a></li>
+    <li><a href="./">Blog</a></li>
+    <li><a href="./">Contact</a></li>
+  </ul></>)
+}
