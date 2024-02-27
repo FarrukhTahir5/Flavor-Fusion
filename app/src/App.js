133c4cc970d46be6c88cf1610cfe47a557fa1f9c
farrukhtahir5 <farrukhtahir5@gmail.com>
2024-02-27 17:32:38 +0500

    updated

--- app/src/App.js
+++ app/src/App.js
@@ -7,6 +7,19 @@ function App() {
     <div className="Header">
       <Header/>
     </div>
+    <div className="main-content">
+      <div className="left-section">
+      <div className="main-text">
+      <span className="heading-main">The ONLY COOKIN'PEDIA You Need!</span><br/>
+      <div className="writing-simple">Elevate Your Cooking Skills</div>
+      </div>
+      <button>View Recipes</button>
+      </div>
+      <div className="right-section">
+        <img src="icon.png" alt="nah"/>
+      </div>
+
+    </div>
 
     </div>
   );
